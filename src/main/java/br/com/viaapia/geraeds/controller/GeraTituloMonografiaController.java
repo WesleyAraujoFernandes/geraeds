70e100208fa1c10cc2a9372c36af9872645c4dc6
WesleyAraujoFernandes <wesley.a.fernandes@gmail.com>
2025-12-17 18:01:20 -0300

    Dependência de log adicionada

--- src/main/java/br/com/viaapia/geraeds/controller/GeraTituloMonografiaController.java
+++ src/main/java/br/com/viaapia/geraeds/controller/GeraTituloMonografiaController.java
@@ -2,6 +2,8 @@ package br.com.viaapia.geraeds.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +15,7 @@ import br.com.viaapia.geraeds.service.GeraTituloMonografiaService;
 @RequestMapping("gerar/monografias")
 public class GeraTituloMonografiaController {
     private final GeraTituloMonografiaService geraTituloMonografiaService;
+    private static final Logger logger = LoggerFactory.getLogger(GeraTituloMonografiaController.class);
 
     public GeraTituloMonografiaController(GeraTituloMonografiaService geraTituloMonografiaService) {
         this.geraTituloMonografiaService = geraTituloMonografiaService;
@@ -22,6 +25,11 @@ public class GeraTituloMonografiaController {
     public Integer monografiasByCodigos(@RequestParam("codigos") List<Integer> codigos) throws Exception {
         if (codigos == null || codigos.size() == 0) {
             throw new Exception("É necessário pelo menos um código para ser gerado!");
+        }
+        if (codigos != null && !codigos.isEmpty()) {
+            logger.info("Iniciando geração de títulos de monografias para os códigos: {}", codigos);
+        } else {
+            logger.info("Iniciando geração de títulos de monografias completa.");
         }
         return this.geraTituloMonografiaService.gerar(codigos);
     }
